4c388b6b1c76d33e59638d5e4948aed1555ece6d
Indrajith Indraprastham <indrajith.indraprastham@go-jek.com>
2023-04-13 13:00:01 +0530

    counter for wedding

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
 
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
-
+  var flipdown = new FlipDown(1688668200, { theme: 'dark' })
     // Start the countdown
     .start()
 
@@ -14,15 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('The countdown has ended!');
     });
 
+  let body = document.body
+  body.classList.toggle('light-theme')
+  body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+  body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
   // Toggle theme
-  var interval = setInterval(() => {
-    let body = document.body;
-    body.classList.toggle('light-theme');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
-  }, 5000);
+  // var interval = setInterval(() => {
+  //   let body = document.body;
+  //   body.classList.toggle('light-theme');
+  //   body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+  //   body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+  // }, 5000);
 
-  // Show version number
-  var ver = document.getElementById('ver');
-  ver.innerHTML = flipdown.version;
 });
